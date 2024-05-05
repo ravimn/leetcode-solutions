92cfe0bfd8ba285c6b11a1f9f49780b3821dda8a
Ravi Namasivayam <ravi_namasivayam@fanniemae.com>
2024-05-05 08:45:36 -0400

    Binary Search - Easy Problem

--- solutions/binarySearch.py
+++ solutions/binarySearch.py
@@ -6,21 +6,21 @@ return -1 if number not found
 
 """
 class Solution:
-    def binarySearch(self, nums: list[int], n: int) -> int:
+    def binarySearch(self, nums: list[int], n: int) -> bool:
         start = 0
         end = len(nums) - 1
 
         #while (start <= end and start >= 0 and end < len(nums)):
-        while (start <= end):
+        while start <= end:
             x = (start + end) // 2
             if (n == nums[x]):
-                return x
+                return True
+
+            if n < nums[x]:
+                end = x - 1
             else:
-                if n < nums[x]:
-                    end = x - 1
-                else:
-                    start = x + 1
+                start = x + 1
         
-        return -1
+        return False
 
 
